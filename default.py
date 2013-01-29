4b9a71bb3ed0d842680be85dc68f35ef81b5744c
Anthirian <gasmelt@gmail.com>
2013-01-29 14:53:40 +0100

    Don't use scan interval when launching manually

--- default.py
+++ default.py
@@ -109,19 +109,10 @@ class Cleaner:
             if self.cleanLibrary and cleaningRequired:
                 # Wait 10 seconds for deletions to finish before cleaning.
                 time.sleep(10)
+                pause = 2
 
-                pause = 5
-                iterations = 0
-                limit = self.scanInterval - pause
                 # Check if the library is being updated before cleaning up
                 while xbmc.getCondVisibility("Library.IsScanningVideo"):
-                    iterations += 1
-
-                    # Make sure we don't mess up the scan interval timing by waiting too long.
-                    if iterations * pause >= limit:
-                        iterations = 0
-                        break
-
                     self.debug("The video library is currently being updated, waiting %d minutes before cleaning up." % pause)
                     time.sleep(pause * 60)
 
